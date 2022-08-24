eb7981897bac57f20d3952859f982028f666aad8
성현 배 <lydiabae0719@gmail.com>
2022-08-24 08:55:35 +0000

    API, DB 연결 제외 프론트 기능 구현 완료

---
+++ 0824성현_education, project/0824_education/Education.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import EducationCard from "./EducationCard";
+import EducationEditForm from "./EducationEditForm";
+import EducationForm from "./EducationForm";
+
+function Education({ education, setEducations, isEditable }) {
+  //useState로 isEditing 상태를 생성함.
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <>
+      {isEditing ? (
+        <EducationEditForm
+          currentEducation={education}
+          setEducations={setEducations}
+          setIsEditing={setIsEditing}
+        />
+      ) : (
+        <EducationCard
+          education={education}
+          isEditable={isEditable}
+          setIsEditing={setIsEditing}
+        />
+      )}
+    </>
+  );
+}
+
+export default Education;
