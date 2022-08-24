6cf79b6359eea788a57724aab11cc6b48dae871b
Sung-Woo Kim <working.zima@gmail.com>
2022-08-24 23:22:02 +0900

    0824-sungwoo-awardFiles

---
+++ awardMVP/AwardAddForm.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Button, Form, Col, Row } from "react-bootstrap";
+import * as Api from "../../api";
+
+function AwardAddForm({ portfolioOwnerId, setAwards, setIsAdding }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const user_id = portfolioOwnerId;
+
+    await Api.post("award/create", {
+      user_id: portfolioOwnerId,
+      title,
+      description,
+    });
+
+    const res = await Api.get("awardlist", user_id);
+    setAwards(res.data);
+    setIsAdding(false);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formBasicTitle">
+        <Form.Control
+          type="text"
+          placeholder="수상내역"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="formBasicDescription" className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="상세내역"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group as={Row} className="mt-3 text-center">
+        <Col sm={{ span: 20 }}>
+          <Button variant="primary" type="submit" className="me-3">
+            확인
+          </Button>
+          <Button variant="secondary" onClick={() => setIsAdding(false)}>
+            취소
+          </Button>
+        </Col>
+      </Form.Group>
+    </Form>
+  );
+}
+
+export default AwardAddForm;
